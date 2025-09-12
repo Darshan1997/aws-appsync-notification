66ee5d480a4ad8c9731f17f43d7921c6f4f01fa0
Darshan <gosai.darshan88@gmail.com>
2025-09-12 16:30:06 +0530

    style: remove comments

--- backend/types/appSyncTypes.ts
+++ backend/types/appSyncTypes.ts
@@ -1,9 +1,6 @@
-// appsync-types.ts
-// A small helper to add type safety to AppSync resolver context
-
 export interface AppSyncContext<TArgs = any, TResult = any> {
-  args: TArgs; // GraphQL arguments
-  result?: TResult; // raw DynamoDB result
-  prev?: { result: TResult }; // previous function result in a pipeline
-  stash: Record<string, any>; // stash for passing data between resolvers
+  args: TArgs;
+  result?: TResult;
+  prev?: { result: TResult };
+  stash: Record<string, any>;
 }
